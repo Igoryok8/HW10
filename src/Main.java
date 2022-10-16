0623b8145c49494553aec0b1b54961a75bebd6e0
Igoryok8 <energymag@mail.ru>
2022-10-17 00:34:45 +0300

    Задания 1 и 2 выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int costs: arr) {
+            sum += costs;
+        }
+        System.out.println("Сумма трат за месяц составила " +sum+ " рублей");
+        System.out.println();
+        System.out.println("Задание 2");
+        int[] arr1 = generateRandomArray();
+        int min = arr1[0];
+        int max = arr1[0];
+        for (int costs: arr1) {
+            if (costs<min) {
+                min=costs;
+            } else if (costs>max) {
+                max=costs;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " +min+ " рублей");
+        System.out.println("Максимальная сумма трат за день составила " +max+ " рублей");
+    }
+
 }
